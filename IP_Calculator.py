8cdbd38558298b9b32f1842b21db83c5d34aea2b
dark <drssvd2@gmail.com>
2020-09-15 14:41:33 +0430

    Add menu and Standard output.

--- IP_Calculator.py
+++ IP_Calculator.py
@@ -1,5 +1,6 @@
 from IP_Tool import Calculate
 
+
 class IP_Calculator:
     def __init__(self, ip, netmask):
         self.ip = ip
@@ -22,18 +23,18 @@ class IP_Calculator:
                 print("IP is not valid.")
                 return False
         
-        if self.netmask.isnumeric() and 0 <= int(self.netmask) <= 32:
+        if self.netmask.isnumeric() and 0 <= int(self.netmask) <= 31:
             is_valid = True
         
         else:
-            print("Netmask range is invalid.(0-32)")
+            print("Netmask range is invalid.(0-31)")
             return False
         
         return is_valid
     
     #Show Binary Form of IP Address
-    def ShowBin(self):
-        bin_ip = [str(bin(int(i)))[2:] for i in self.listed_ip]
+    def ShowBin(self, ip):
+        bin_ip = [str(bin(int(i)))[2:] for i in ip.split('.')]
         s=[]
         for i in range(len(bin_ip)):
             if len(bin_ip[i])!=8:
@@ -72,7 +73,6 @@ class IP_Calculator:
         wildcard = [str(255 - int(listed_ip[i])) for i in range(0,4)]
         return(('.'.join(listed_ip)),'.'.join(wildcard))
     
-    
     #Calculate The network, broadcast, min and max ip 
     def IP_Details(self):
         ip_listed =[int(i) for i in self.listed_ip]
@@ -90,7 +90,43 @@ class IP_Calculator:
         elif 24 <= netmask <= 31:
             return(Calculate(netmask, ip_listed,4))
     
+   
+def Print_With_Indentation(message, ip, binary='') :
+    max_indent = 70
+    a = len(message)
+    b = len(ip)
+    c = len(binary)
+    d = int(a+b+c)
+    indent = int(max_indent - d)
+    f = 17-len(message)
+    print(message,(' '*f),ip,(" "*(70-(d+f))),binary,"|")
+    
+def menu():
+    print("-"*76)
+    print("-"*27," IP Calculator v1.0 ","-"*27)
+    print("-"*24,"  Website : bambootech.ir ",24*"-")
+    print("-"*27," Creator : Salvatore","-"*27)
+    print("-"*76)
+    
 if __name__ == '__main__':
-    test = IP_Calculator('0.255.255.0','23')
-    print(test.IP_Details())
+    menu()
+    ip = input("IP-Address# ")
+    netmask =input("Netmask(0-31)# ")
+    data = IP_Calculator(ip,netmask)
+    if data.Validator(): 
+        print("-"*76)
+        Print_With_Indentation("| Address:", ip,data.ShowBin(ip))
+        print("| Class of IP:",data.IP_Class())
+        print("-"*76)
+        Print_With_Indentation("| Netmask:",data.NetMask_Wildcard()[0],data.ShowBin(data.NetMask_Wildcard()[0]))
+        Print_With_Indentation("| Wildcard:",data.NetMask_Wildcard()[1],data.ShowBin(data.NetMask_Wildcard()[1]))
+        print("-"*76)
+        Print_With_Indentation("| Network:",data.IP_Details()[0],data.ShowBin(data.IP_Details()[0]))
+        Print_With_Indentation("| Broadcast:",data.IP_Details()[1],data.ShowBin(data.IP_Details()[1]))
+        print("-"*76)
+        Print_With_Indentation("| Host Min:",data.IP_Details()[2],data.ShowBin(data.IP_Details()[2]))
+        Print_With_Indentation("| Host Max:",data.IP_Details()[3],data.ShowBin(data.IP_Details()[3]))
+        print("| Hosts: ",data.Show_Hosts())
+        print("-"*76)
+    
     
