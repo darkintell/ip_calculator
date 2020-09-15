d5640f418d2c17ee001e6dac642d574337929f24
dark <drssvd2@gmail.com>
2020-09-15 12:20:29 +0430

    Show the class of ip (A-E).

--- IP_Calculator.py
+++ IP_Calculator.py
@@ -44,10 +44,23 @@ class IP_Calculator:
     def Show_Hosts(self):
         return 2**(32 - int(self.netmask)) - 2
     
-    
+    #
+    def IP_Class(self):
+        ip_listed = self.listed_ip
+        first = int(ip_listed[0])
+        if  0<= first <= 127:
+            return 'A'
+        elif 128 <= first <= 191:
+            return 'B'
+        elif 192 <= first <= 223:
+            return 'C'
+        elif 224 <= first <= 239:
+            return 'D'
+        elif 240 <= first <= 255:
+            return 'E'
             
         
 if __name__ == '__main__':
-    test = IP_Calculator('255.255.255.0','21')
-    print(test.Show_Hosts())
+    test = IP_Calculator('0.255.255.0','')
+    print(test.IP_Class())
     
